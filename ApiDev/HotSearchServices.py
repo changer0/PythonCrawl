fbd8e3b0fe66307729df9e6a1abdeb7b50347ae2
zhanglulu <zhanglulu@yuewen.com>
2021-02-14 22:06:05 +0800

    添加时间戳

--- ApiDev/HotSearchServices.py
+++ ApiDev/HotSearchServices.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/hotSearch',methods=["GET"])
 def hotSearch():
     # 默认返回内容
-    return_dict= {'code': 200, 'msg': '处理成功',  "expired_time": -1, 'result': False}
+    return_dict= {'code': 200, 'msg': '处理成功',  "expired_time": -1, 'time_stamp': -1, 'result': False}
     
     # 获取传入的params参数
     get_data=request.args.to_dict()
@@ -24,9 +24,10 @@ def hotSearch():
     else:
         return_dict['msg']="微博爬取失败！"
         return_dict['code']=-1
-    # 过期时间，用于做客户端缓存 毫秒单位, 目前过期时间为 1 分钟
-    return_dict['expired_time'] = (time.time() + 60) * 1000
-
+    # 过期时间，用于做客户端缓存 毫秒单位, 目前过期时间为 10s
+    return_dict['expired_time'] = (time.time() + 10) * 1000
+    return_dict['time_stamp'] = time.time() * 1000
+    
     return json.dumps(return_dict, ensure_ascii=False)
 
 if __name__ == '__main__':
